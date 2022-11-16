b73f5701368484a2b440a257da7979201727a362
Jessica Mosby <jessicamosby@mac.com>
2022-11-16 15:35:35 -0500

    added signIn and signUp forms in app.js

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+import { signInUser, signUpUser } from "./fetch-utils.js";
+import {};
+
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
+
 // Wire up sign in and sign up forms to supabase
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const email = data.get('email');
+    const user = await signInUser(email, data.get('password'));
+
+    if (user) {
+        location.replace('/other-page');
+    }
+});
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const email = data.get('email');
+    const user = await signUpUser(email, data.get('password'));
+
+    if (user) {
+        location.replace('/other-page');
+    }
+});
+
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
